4e077900a0118975aea6ffaeb40b283159bdb014
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-06-15 03:26:00 -0400

    complete lab

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { directors } from "../data";
 
-function Directors() {
-  return <div>{/*{code here}*/}</div>;
-}
+const Directors = () => {
+  const directorPage = directors.map((director) => (
+    <div key={director.name}>
+      <h2>{director.name}</h2>
+      <ul>
+        {director.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directorPage}
+    </div>
+  );
+};
 
 export default Directors;
